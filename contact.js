d386754de3c3ddeb22cf6330847bb6021e838d3e
vodiaz1987 <valeria.diaz.19@ucl.ac.uk>
2020-03-11 19:27:14 +0000

    BUTTON

--- contact.js
+++ contact.js
@@ -1,12 +1,22 @@
+
 var name = document.getElementById("Name").value;
 var email = document.getElementById("Email").value;
 var message = document.getElementById("Message").value;
-if (!name) {
-  alert ("Enter your name")
-} else if (!email) {
-  alert ("Enter your email")
-} else if (!message) {
-  alert ("Enter a message")
-} else {
-  alert ("Thank you for contact us, We will reply as soon as we can")
+
+function submitInput(){
+  return  $("contact-form submit").id;
 }
+if (submitInput()){
+
+  if (!name) {
+    alert ("Enter your name")
+  } else if (!email) {
+    alert ("Enter your email")
+  } else if (!message) {
+    alert ("Enter a message")
+  } else {
+    alert ("Thank you for contact us, We will reply as soon as we can")
+  }
+}
+
+
