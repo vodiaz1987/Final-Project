d9c6198f49c143193d2abc2bbb93c070353b3cfc
vodiaz1987 <valeria.diaz.19@ucl.ac.uk>
2020-03-11 10:59:42 +0000

    contact.js

--- contact.js
+++ contact.js
@@ -1,7 +1,12 @@
-
-// if($(check).is(`:checked`)){
-//     $(`form`).submit();
-// }
-// else{
-//     alert('Before Submit, you need to check the checkbox')
-// }
+var name = document.getElementById("Name").value;
+var email = document.getElementById("Email").value;
+var message = document.getElementById("Message").value;
+if (!name) {
+  alert ("Enter your name")
+} else if (!email) {
+  alert ("Enter your email")
+} else if (!message) {
+  alert ("Enter a message")
+} else {
+  alert ("Thank you for contact us, We will reply as soon as we can")
+}
